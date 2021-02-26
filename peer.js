fc9f2bb073b9474facd78b4dcd33633d22bac11c
mccooll <github@no3.ca>
2021-02-26 16:37:35 -0500

    attempt to add compression layer

--- peer.js
+++ peer.js
@@ -7,6 +7,7 @@ const process = require('process');
 //const { pipeline } = require('stream');
 const pipe = require('it-pipe');
 const concat = require('it-concat');
+const zopfli = require('node-zopfli');
 
 const main = async () => {
     const peer = await Libp2p.create({
@@ -27,22 +28,37 @@ const main = async () => {
     peer.multiaddrs.forEach(addr => console.log(`${addr.toString()}/p2p/${peer.peerId.toB58String()}`));
 
     peer.handle('/zoppy', async ({stream, connection}) => {
-        const input = await pipe(
+        const compressed = await pipe(
+            stream,
+            concat,
+            zopfli.createGzip
+        );
+        console.log(compressed.toString());
+        console.log(connection.remoteAddr);
+        console.log(connection.remotePeer.toB58String());
+        const dialBack = `${connection.remoteAddr.toString()}/p2p/${connection.remotePeer.toB58String()}`;
+        console.log('send compressed data');
+        const { stream2 } = await peer.dialProtocol(multiaddr(dialBack), '/zopped');
+        await pipe(
+            [compressed.toString()],
+            stream2
+        );
+    });
+
+    peer.handle('/zopped', async ({stream}) => {
+        const compressed = await pipe(
             stream,
             concat
         );
-        console.log(input.toString());
-        console.log(connection.remoteAddr);
-        console.log(connection.remotePeer.toB58String());
+        console.log('receive compressed data');
+        console.log(compressed.toString());
     });
-
-    //peer.handle('/zopped')
 
     console.log('ping try');
     process.argv[2] ? console.log(await peer.ping(multiaddr(process.argv[2]))) : console.log('not pinging');
 
     if(process.argv[2]) {
-        console.log('send unencrypted data');
+        console.log('send uncompressed data');
         const { stream } = await peer.dialProtocol(multiaddr(process.argv[2]), '/zoppy');
         await pipe(
             ['Hello p2p', ' ', 'world!'],
